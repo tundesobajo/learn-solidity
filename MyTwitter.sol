67e4c0109cab0038d504ed085c54825576c56557
Olatunde Sobajo <6509610+tundesobajo@users.noreply.github.com>
2024-12-18 00:49:04 +0100

    Add functions to like and unlike tweets

--- MyTwitter.sol
+++ MyTwitter.sol
@@ -5,6 +5,7 @@ contract MyTwitter {
     address public owner;
     uint16 public MAX_TWEET_LENGTH = 280;
     struct Tweet {
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
@@ -21,10 +22,15 @@ contract MyTwitter {
         _;
     }
 
+    function changeMaxTweetLength(uint16 length) public onlyOwner {
+        MAX_TWEET_LENGTH = length;
+    }
+
     function createTweet(string memory _tweet) public {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet is too long");
 
         Tweet memory newTweet = Tweet({
+            id: tweets[msg.sender].length,
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
@@ -42,8 +48,15 @@ contract MyTwitter {
         return tweets[_owner];
     }
 
-    function changeMaxTweetLength(uint16 length) public onlyOwner {
-        MAX_TWEET_LENGTH = length;
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "UNAVAILABLE: Tweet does not exist");
+        tweets[author][id].likes++;
+    }
+
+    function unlikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "UNAVAILABLE: Tweet does not exist");
+        require(tweets[author][id].likes > 0, "BAD REQUEST: There are no likes");
+        tweets[author][id].likes--;
     }
 
 }
